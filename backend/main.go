a0a8cb6ead96f3a1564e2845f5828a275a62947c
Pump <patsanop@gmail.com>
2024-12-04 23:43:32 +0700

    update router cors config

--- backend/main.go
+++ backend/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/pump-p/solidithai-assignment-2/backend/database"
 	"github.com/pump-p/solidithai-assignment-2/backend/routes"
@@ -14,17 +13,9 @@ func main() {
 	// Set up the router
 	r := gin.Default()
 
-	// CORS middleware
-	r.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:3000", "http://localhost:3001"},
-		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE"},            // Allowed HTTP methods
-		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"}, // Allowed headers
-		AllowCredentials: true,                                                // Allow cookies/authentication
-	}))
-
 	// Register routes
 	routes.SetupRouter(r)
 
 	// Start the server
-	r.Run(":5555")
+	r.Run(":8080")
 }
--- backend/routes/routes.go
+++ backend/routes/routes.go
@@ -1,12 +1,33 @@
 package routes
 
 import (
+	"time"
+
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/pump-p/solidithai-assignment-2/backend/controllers"
 	"github.com/pump-p/solidithai-assignment-2/backend/middlewares"
 )
 
 func SetupRouter(router *gin.Engine) {
+
+	// CORS middleware
+	// router.Use(cors.New(cors.Config{
+	// 	AllowOrigins:     []string{"http://localhost:3000"},
+	// 	AllowMethods:     []string{"GET", "POST", "PUT", "DELETE"},
+	// 	AllowHeaders:     []string{"Origin", "Content-Type", "Authorization", "Accpet"},
+	// 	ExposeHeaders:    []string{"Content-Length", "Authorization"},
+	// 	AllowCredentials: true,
+	// 	MaxAge:           12 * time.Hour,
+	// }))
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"*"},
+		AllowMethods:     []string{"*"},
+		AllowHeaders:     []string{"*"},
+		ExposeHeaders:    []string{"*"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
 
 	authRoutes := router.Group("/auth")
 	{
@@ -18,7 +39,7 @@ func SetupRouter(router *gin.Engine) {
 	{
 		userRoutes.GET("/", controllers.GetUsers)
 		userRoutes.GET("/:id", controllers.GetUserByID)
-		userRoutes.POST("/", controllers.CreateUser)
+		userRoutes.POST("", controllers.CreateUser)
 		userRoutes.PUT("/:id", controllers.UpdateUser)
 		userRoutes.DELETE("/:id", controllers.DeleteUser)
 	}
